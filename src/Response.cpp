ae67a6a5cd78f9e2b61702ea1e6eb3f9fa2a766c
Watcher Jarr <wjarr@et-l4.kzn.21-school.ru>
2021-07-09 12:49:30 +0300

    add new files

---
+++ src/Response.cpp
@@ -0,0 +1,8 @@
+//                                   //
+// Created by:                       //
+//              Watcher Jarr         //
+//         on:                       //
+//              7/9/21				 //
+//                                   //
+
+#include "Response.hpp"
---
+++ src/Response.hpp
@@ -0,0 +1,17 @@
+//                                   //
+// Created by:                       //
+//              Watcher Jarr         //
+//         on:                       //
+//              7/9/21				 //
+//                                   //
+
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+
+class Response{
+
+};
+
+
+#endif //RESPONSE_HPP
