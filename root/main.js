c41b27aa854b6800e1588af92447a116aae162fc
Watcher Jarr <wjarr@et-k2.kzn.21-school.ru>
2021-08-03 13:01:05 +0300

    upgrade pages

--- root/main.js
+++ root/main.js
@@ -5,6 +5,26 @@ const body = document.body;
 
 body.style.backgroundImage = `url(${image[rand]})`;
 
+const signs = document.getElementsByClassName("signs");
+const links = document.getElementsByTagName("a");
+
+if (rand === 1){
+    for (let link of links) {
+        link.style.color = "aqua";
+    }
+    for (let sign of signs) {
+        sign.style.color = "navajowhite";
+    }
+}
+else {
+    for (let link of links) {
+        link.style.color = "yellow";
+    }
+    for (let sign of signs) {
+        sign.style.color = "darkblue";
+    }
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
