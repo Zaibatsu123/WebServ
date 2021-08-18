adf78751a5877618d0ad5246b70b4ac8407d727b
Jolene Radioactive <jradioac@et-k4.kzn.21-school.ru>
2021-08-18 20:46:29 +0300

    commit

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,7 +12,7 @@ Server::Server()
     // sockets.push_back(*socket);
     this->server_name = "127.0.0.1";
     // this->autoindex = 1;
-    this->max_body_size = 1024;
+    // this->max_body_size = 1024;
     // this->locations.insert(std::pair<std::string, std::string>("/", "./root"));
 	this->error_pages.insert(std::pair<int, std::string>(400, "./root/errorPages/400.html"));
 	this->error_pages.insert(std::pair<int, std::string>(403, "./root/errorPages/403.html"));
